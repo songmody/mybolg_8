dbc7af9ffbb8141d972cbb1a73febfbf19b4f21a
songmody <tnejd4766@naver.com>
2019-08-03 21:38:07 +0900

    hh

--- heroku/settings.py
+++ heroku/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x+#^pri=2g#2l#sp1@6@q$578s=r@6ic1q$&#=ysd+xx$m+!64'
+SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY','x+#^pri=2g#2l#sp1@6@q$578s=r@6ic1q$&#=ysd+xx$m+!64')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool (os.environ.get('DJANGO_DEBUG',True))
 
 ALLOWED_HOSTS = ['*']
 
--- heroku/urls.py
+++ heroku/urls.py
@@ -6,14 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/<int:blog_id>',blogapp.views.finish,name="finish"),
-    path('',blogapp.views.home,name="home"),#home이라는주소생성
-    path('blog/drink/',blogapp.views.drink, name="drink"),
-    path('blog/food/',blogapp.views.food, name="food"),
-    path('blog/fashion/',blogapp.views.fashion, name="fashion"),
-    path('blog/specialprice/',blogapp.views.specialprice, name="specialprice"),
-    path('blog/message/',blogapp.views.message, name="message"),
-    path('blog/create',blogapp.views.create, name="create"),
+    path('heroku/<int:blog_id>',heroku.views.finish,name="finish"),
+    path('',heroku.views.home,name="home"),#home이라는주소생성
+    path('heroku/drink/',heroku.views.drink, name="drink"),
+    path('heroku/food/',heroku.views.food, name="food"),
+    path('heroku/fashion/',heroku.views.fashion, name="fashion"),
+    path('heroku/specialprice/',heroku.views.specialprice, name="specialprice"),
+    path('heroku/message/',heroku.views.message, name="message"),
+    path('heroku/create',heroku.views.create, name="create"),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
  
