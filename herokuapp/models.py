b4def16f838e38df96d0c8089b7a4c1783c71cb8
songmody <tnejd4766@naver.com>
2019-08-03 22:03:03 +0900

    kk

--- herokuapp/models.py
+++ herokuapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 # 클래스로써 어떤 데이터의 종류를 처리할지 적는 공간
-class Blog(models.Model):
+class Heroku(models.Model):
     title = models.CharField(max_length=200) #title이라는 변수에 짧은 문장(charfield)를 저장한다.
     image = models.ImageField(upload_to='images/')# 정형화된 시간데이터를 사용하겠
     description = models.CharField(max_length=500) #title보다 긴 텍스트 데이터를 사용
